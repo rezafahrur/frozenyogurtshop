a5ed4047031979604d02368ca21d7695e7f23af1
rezafahrur <rezafahrurrasyid@gmail.com>
2018-09-27 13:10:30 +0700

    initial commit

---
+++ database/migrations/2018_09_27_060900_buat_table_produks.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BuatTableProduks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama_produk');
+            $table->string('image');
+            $table->unsignedInteger('id_kategori');
+            $table->foreign('id_kategori')
+                  ->references('id')
+                  ->on('kategoris')
+                  ->onDelete('cascade'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produks');
+    }
+}
